1414372dc808cd63b901411cf5ecee760b46876a
Rakshit Poojari <Rakshitpo54@gmail.com>
2023-08-29 11:21:40 +0530

    finish

--- Weather_App/script.js
+++ Weather_App/script.js
@@ -1,44 +1,71 @@
+// app.js
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const cityElement = document.querySelector(".city");
+const tempElement = document.querySelector(".temp");
+const humidityElement = document.querySelector(".humidity");
+const windElement = document.querySelector(".wind");
+const weatherIcon = document.querySelector(".weather-icon");
 
-  const apiKey = "f04d54732055581e9c51ed28d4d78937";
-  const searchBox = document.querySelector(".search input");
-  const searchBtn = document.querySelector(".search button");
-  const cityElement = document.querySelector(".city");
-  const tempElement = document.querySelector(".temp");
-  const humidityElement = document.querySelector(".humidity");
-  const windElement = document.querySelector(".wind");
-  const weatherIcon = document.querySelector(".weather-icon");
-  
-  const weatherIcons = {
-    clouds: "images/clouds.png",
-    clear: "images/clear.png",
-    rain: "images/rain.png",
-    drizzle: "images/drizzle.png",
-    mist: "images/Mist.png"
-  };
-  
-  let debounceTimer;
-  searchBtn.addEventListener("click", () => {
-    clearTimeout(debounceTimer);
-    debounceTimer = setTimeout(() => {
-      checkWeather(searchBox.value);
-    }, 300);
-  });
-  
-  async function checkWeather(city) {
-    try {
-      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-  
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-  
-      cityElement.innerHTML = data.name;
-      tempElement.innerHTML = Math.round(data.main.temp) + "°C";
-      humidityElement.innerHTML = data.main.humidity + "%";
-      windElement.innerHTML = data.wind.speed + "km/h";
-  
-      weatherIcon.src = weatherIcons[data.weather[0].main.toLowerCase()] || "default.png";
-    } catch (error) {
-      console.error(error);
-      // Handle the error, e.g., display an error message to the user
+const weatherIcons = {
+  clouds: "images/clouds.png",
+  clear: "images/clear.png",
+  rain: "images/rain.png",
+  drizzle: "images/drizzle.png",
+  mist: "images/Mist.png",
+};
+
+let debounceTimer;
+searchBtn.addEventListener("click", () => {
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(() => {
+    const city = searchBox.value.trim();
+    if (city) {
+      checkWeather(city);
     }
+  }, 300);
+});
 
+async function checkWeather(city) {
+  try {
+    const apiKey = "f04d54732055581e9c51ed28d4d78937"; // Replace with your API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (response.status === 404) {
+      showError("City not found");
+    } else {
+      hideError();
+      updateWeatherInfo(data);
+    }
+  } catch (error) {
+    console.error(error);
+    showError("An error occurred. Please try again later.");
+  }
+}
+
+function updateWeatherInfo(data) {
+  cityElement.textContent = data.name;
+  tempElement.textContent = Math.round(data.main.temp) + "°C";
+  humidityElement.textContent = data.main.humidity + "%";
+  windElement.textContent = data.wind.speed + "km/h";
+
+  const weatherMain = data.weather[0].main.toLowerCase();
+  weatherIcon.src = weatherIcons[weatherMain] || "default.png";
+}
+
+function showError(message) {
+  document.querySelector(".error").style.display = "block";
+  document.querySelector(".weather").style.display = "none";
+  document.querySelector(".error").textContent = message;
+}
+
+function hideError() {
+  document.querySelector(".error").style.display = "none";
+  document.querySelector(".weather").style.display = "block";
+}
+
+// Display initial weather on page load (optional)
+// checkWeather("DefaultCity"); // Replace "DefaultCity" with a default city
