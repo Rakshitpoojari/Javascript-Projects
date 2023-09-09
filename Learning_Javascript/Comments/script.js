43afb5c8ffc2171376815cec99046a32797aa282
Rakshit Poojari <Rakshitpo54@gmail.com>
2023-09-09 10:49:29 +0530

    sss

---
+++ Learning_Javascript/Comments/script.js
@@ -0,0 +1,18 @@
+// Showing the Comments for understanding the developer
+console.clear();
+
+/* MultiLine Commment
+1 2 3
+22
+33
+
+
+*/
+// Assert
+const val=5;
+console.assert(val===5,'Value is 5');
+
+
+// adding styles to the console
+const styles="background-color:white; color:black; padding:20px;"
+console.log("%cHello world",styles)
--- Learning_Javascript/Console/script.js
+++ Learning_Javascript/Console/script.js
@@ -92,3 +92,4 @@ console.assert(val===5,'Value is 5');
 // adding styles to the console
 const styles="background-color:white; color:black; padding:20px;"
 console.log("%cHello world",styles)
+
