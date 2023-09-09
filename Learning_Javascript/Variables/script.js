f16a8b85d0a7419f55d06252e79b784cece819f1
Rakshit Poojari <Rakshitpo54@gmail.com>
2023-09-09 11:22:55 +0530

    Variable and datatyes

---
+++ Learning_Javascript/Variables/script.js
@@ -0,0 +1,26 @@
+// Ways to declare Variables
+// var ,let ,const
+
+let firstName='Rakshit'
+let lastName='Poojari'
+
+console.log(firstName,lastName)
+// console.log(age)
+
+let age=22
+console.log(typeof firstName)
+console.log(typeof lastName)
+console.log(typeof age)
+console.log(age)
+
+
+// Datatypes
+// string
+// String firstName1='Rakshit'
+const uniquesymbol=Symbol("description")
+console.log(uniquesymbol)
+
+let name='Rakshit'
+let ages;
+let score=11;
+let player1score=null;
